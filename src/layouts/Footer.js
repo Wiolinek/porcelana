cad117a8c2deb307d3afa26a39be6c305d1422cb
Wiola P <wiol@onet.eu>
2020-12-06 18:15:02 +0100

    add contact form

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,15 +2,30 @@ import React from 'react';
 
 import '../styles/Footer.css';
 
+
 const Footer = () => {
 
     return (
-        <footer>
-            <div className="contact">
-            <p>Kontakt formularz, International Ceramics Manufacturing Inc.</p>
+        <div className="contact">
+            <div className="contact_form">
+                <p>Contact Us!</p>
+            <form>
+                <label>Your name:
+                <input type="text" required></input>
+                </label>
+                <label>Your surname:
+                <input type="text" required></input>
+                </label>
+                <label>Your email address:
+                <input type="email"></input>
+                </label>
+                <label>Your message:
+                <input type="text"></input>
+                </label>
+            </form>
             </div>
-            <div className="contribution">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> 
-        </footer>
+            <div className="contribution">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>   
+        </div> 
     )
 }
 
