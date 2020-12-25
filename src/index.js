2915ddb407b95456df83c3bcb2515d71f329069a
Wiola P <wiol@onet.eu>
2020-12-25 17:18:52 +0100

    style shop products

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import App from './layouts/App';
 import reportWebVitals from './reportWebVitals';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+<App />
+</React.StrictMode>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
