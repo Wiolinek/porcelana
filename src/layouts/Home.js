b7c28aa93ca2c39853dcf8cb947d7e3202bcdd35
Wiola P <wiol@onet.eu>
2021-01-13 00:30:30 +0100

    start implementing styled components

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,21 +1,61 @@
 import React from 'react';
 
-import '../styles/Home.css';
+import styled from 'styled-components';
 
 const Home = () => {
 
     return (
     <>
-        <section className ="header">
-        <div className="tagline">
+        <HomeHeader>
+        <div>
             <p>Our design</p>
             <p>Your <span>style</span></p>
         </div>
-        <button className="arrow"><i class="fas fa-angle-double-down"></i></button>
-        </section>
+        <button><i class="fas fa-angle-double-down"></i></button>
+        </HomeHeader>
     </>
     )
 }   
 
+const HomeHeader = styled.section`
+    height: 100vh;
+
+div {
+   display: block;
+   position: absolute;
+   top: 30vh;
+   left: 16vw;
+   width: 60vw;
+   font-size: 130px;
+   font-family: 'Raleway Dots', cursive;
+   text-transform: uppercase;
+   letter-spacing: .3vw;
+   line-height: 180px;
+ }
+
+div span {
+    font-family: 'Unica One', cursive;
+    font-size: 155px;
+    text-transform: lowercase;
+    letter-spacing: .9vw;
+    }
+
+div p:last-child {
+    padding-top: 3vh;
+    padding-left: 5vw;
+ }
+
+button {
+    position: absolute;
+    bottom: 3vh;
+    right: calc(50vw - 35px);
+    height: 100px;
+    width: 70px;
+    border: none;
+    background-color: rgba(255,255,255,0);
+    align-content: center;
+    outline: 0;
+    }
+`;
 
 export default Home;
--- src/layouts/Modal.js
+++ src/layouts/Modal.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { jobOffersData } from '../../src/data_career.js';
-import { Link } from 'react-scroll'
+import { Link } from 'react-scroll';
 
 // import {Link} from 'react-router-dom';
-// import { Modal } from 'react-responsive-modal';
 
-// import '../styles/Articles.css';
-import '../styles/Modal.css';
+import styled from 'styled-components';
 // import 'react-responsive-modal/styles.css';
 
 
@@ -20,11 +18,11 @@ const Modal = ({ state, offer, closeModalHandler }) => {
     let { name, location, department, employmentType, positionDescription, positionRequirements, personalRequirements, whatWeOffer } = index; //elements selected clicked object in jobOffersData array
 
         return (
-            <div className="modal-wrapper" style={{
+            <ModalWrapper style={{
                 display: state ? 'block' : 'none',
                 // transform: state ? 'translateY(0vh)' : 'translateY(-100vh)',
             }}>
-                <button className="close-btn" onClick={closeModalHandler}>Close</button>
+                <button onClick={closeModalHandler}>Close</button>
                 <div>
                 <h2>{name}</h2>
                 <p><span>Location: </span><span>{location}</span></p>
@@ -36,8 +34,70 @@ const Modal = ({ state, offer, closeModalHandler }) => {
                 <p><span>What We Offer: </span><span>{whatWeOffer}</span></p>
                 </div>
                 <div className="send-cv-btn"><Link to="contact" smooth={true} onClick={closeModalHandler}>Send CV</Link></div>
-            </div>
+            </ModalWrapper>
     )
 }
+
+const ModalWrapper = styled.div`
+    margin: 0 auto;
+    display: none;
+    position: fixed;
+    top: 3vh;
+    left: 10vw;
+    background-color: #d0d0dc;
+    height: 94vh;
+    width: 80vw;
+    padding: 0 2vw;
+    overflow: auto;
+        /* transition: all 1.3s ease; */
+
+span:first-child {
+    font-weight: 700;
+    font-size: 19px;
+}
+
+div {
+    font-size: 19px;
+}
+
+div p {
+    padding-top: 1.3vh;
+}
+
+.send-cv-btn {
+    border: 3px solid #d0d0dc;
+    color: #d0d0dc;
+    text-shadow: 2px 2px 3px black;
+    font-size: 22px;
+    border-radius: 50%;
+    margin: 3vh 3vh 3vh auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    width: 115px;
+    height: 115px;
+    outline: none;
+}
+  
+button {
+    font-family: 'Cinzel', serif;
+    border: 3px solid #d0d0dc;
+    color: #d0d0dc;
+    text-shadow: 2px 2px 3px black;
+    font-size: 17px;
+    border-radius: 50%;
+    margin: 3vh 3vh 1vh auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    width: 70px;
+    height: 70px;
+    outline: none;
+}
+
+
+`;
 
 export default Modal;
