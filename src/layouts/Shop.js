48e0ead064019d88c8c31aae3e0ec51623810c54
Wiola P <wiol@onet.eu>
2021-01-29 22:56:37 +0100

    implem. addToWaitingRoomHandl & addToCartHandl #2

--- src/layouts/Shop.js
+++ src/layouts/Shop.js
@@ -9,8 +9,6 @@ import productsData from '../../src/data_products.js';
 
 const Shop = () => {
 
-    let { item } = productsData; //ta destrukturyzacja działa ?
-
     let [searchText, setSearchText] = useState("");
     let [optionSelected, setOptionSelected] = useState("All");
     let [cartProductsList, setCartProductsList] = useState([]);
@@ -29,22 +27,42 @@ const Shop = () => {
     const addToWaitingRoomHandler = (e, item) => {
         let id = e.target.id;
         let quantity = e.target.value;
-        let inCartProduct = waitingRoom.find(item => item.id === e.target.id); //check if product is in waiting room already
+        let isInWaitingRoom = waitingRoom.find(item => item.id === e.target.id); //check if product is in waiting room already
 
-        if(inCartProduct) { // if product is in waiting room increase quantity only
-            setWaitingRoom(waitingRoom.map(item => item.id === id ? {...inCartProduct, quantity: e.target.value} : item))
-        } else { //if product is not in waiting room - add it and update waiting room
+        if(isInWaitingRoom) { // if product is in waiting room - change quantity only
+            setWaitingRoom(waitingRoom.map(item => item.id === id ? {...isInWaitingRoom, quantity: quantity} : item));
+        } else if(isInWaitingRoom && item.quantity === '0'){
+
+            //SPRAWDZIĆ TE OPCJE, NIE DZIAŁA WYWALENIE Z POCZEKALNI INDEKSÓW Z ILOŚCIĄ 0
+
+            // setWaitingRoom(waitingRoom.filter(item => item.quantity !== '0' ? waitingRoom : null));
+            setWaitingRoom(waitingRoom.filter(item => item.quantity > '0'));
+            // setWaitingRoom(waitingRoom.filter(item => item.quantity !== '0' ? waitingRoom : null));
+
+
+
+        } else if(!isInWaitingRoom){ //if product is not in waiting room - add it and update waiting room
             setWaitingRoom([...waitingRoom, {id, quantity}]);
-            console.log(waitingRoom);
-        }
+        }  
     }
 
     const addToCartHandler = (e, item) => {
-        let addedToCartItem = waitingRoom.find(item => item.id === e.target.id); //looking for product in waiting room and fetching quantity
-        if(addedToCartItem) { //if product is in waiting room add to cart
-            setCartProductsList([...cartProductsList, addedToCartItem]);
+        let isInWaitingRoom = waitingRoom.find(item => item.id === e.target.id); //looking for product in waiting room and fetching quantity
+        
+
+        if(isInWaitingRoom) { //if product is in waiting room add to cart
+            let isInCart = cartProductsList.find(item => item.id === e.target.id);
+            let id = waitingRoom.id; // fetching product ID from waiting room
+            let quantity = waitingRoom.quantity; // fetching product quantity from waiting room
+
+            setCartProductsList([...cartProductsList, isInWaitingRoom]);
             setWaitingRoom(waitingRoom.filter(item => item.id !== e.target.id ? waitingRoom : null)); //delete added to cart product from waiting room
             //DODAĆ ZEROWANIE INPUTA PO WRZUCENIU PRODUKTU DO KOSZYKA
+
+            // DODAWANIE OBU STANÓW - ZROBIĆ
+            if(isInCart){ // if product is already in cart add quantity from cart and quantity from waiting room
+                setCartProductsList(cartProductsList.map(item => item.id === id ? {...isInCart, quantity: cartProductsList.quantity + waitingRoom.quantity} : item));
+            }
         } else {
             //do nothing if product is not in waiting room - it means qantity is not been chosen so equals 0
         }  
