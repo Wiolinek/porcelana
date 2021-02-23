05621947ed85f80375b2a44e2bce36057e8b9a95
Wiola P <wiol@onet.eu>
2021-02-23 16:52:57 +0100

    recommendations section animation improve

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -51,16 +51,16 @@ const Footer = () => {
                 </div>
                 <div className="form">
                     <form>
-                        <label htmlFor="user_name">Name:<br/>
-                        <input type="text" id="user_name" name="user_name" required></input></label>
-                        <label htmlFor="user_surname">Surname:<br/>
-                        <input type="text" id="user_surname" name="user_surname" required></input></label>
-                        <label htmlFor="user_email">E-mail:<br/>
-                        <input type="email" id="user_email" name="user_email" required></input></label>
-                        <label htmlFor="user_message">Message:<br/>
-                        <textarea type="text" id="user_message" name="user_message" required></textarea></label>
-                        <label htmlFor="user_file">Add file:<br/>
-                        <input type="file" id="user_file" name="user_file"></input></label>
+                        <label htmlFor="user_name">Name:</label>
+                        <input type="text" id="user_name" name="user_name" required></input>
+                        <label htmlFor="user_surname">Surname:</label>
+                        <input type="text" id="user_surname" name="user_surname" required></input>
+                        <label htmlFor="user_email">E-mail:</label>
+                        <input type="email" id="user_email" name="user_email" required></input>
+                        <label htmlFor="user_message">Message:</label>
+                        <textarea type="text" id="user_message" name="user_message" required></textarea>
+                        <label htmlFor="user_file">Add file:</label>
+                        <input type="file" id="user_file" name="user_file"></input>
                         <input type="submit" value="Send"></input>
                     </form>
                 </div>
--- src/layouts/Recommendations.js
+++ src/layouts/Recommendations.js
@@ -16,10 +16,10 @@ const Recommendations = () => {
 
     useEffect(() => {
   
-        gsap.fromTo(opinionsWrapper.current.children, {y: '+=170', autoAlpha: 0}, {y: 0, autoAlpha: 1, stagger: .7, duration: 2,
+        gsap.fromTo(opinionsWrapper.current.children, {y: '+=170', scale: 1.4, autoAlpha: 0}, {y: 0, scale: 1, autoAlpha: 1, stagger: .7, duration: 2, ease: "power1.inOut",
           scrollTrigger: {
             trigger: opinionsWrapper.current,
-            start:'top 65%',
+            start:'top 55%',
             end:'bottom bottom',
             scrub: 0,
             // pin: true,
