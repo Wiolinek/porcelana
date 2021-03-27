09749cdbc895b88777f0e882157ed9d56c879f82
Wiola P <wiol@onet.eu>
2021-03-27 20:37:47 +0100

    scroll to the top on logo

--- src/components/main/Home.js
+++ src/components/main/Home.js
@@ -25,13 +25,13 @@ const Home = () => {
 
     useEffect(() => {
 
-        const tl = gsap.timeline()
+        const tl = gsap.timeline();
 
-        tl 
+        tl
+            .from(logoAnimation.current, {scale: 0,  delay: .3})
             .to(homeWrapper.current, {opacity: 1, duration: 3, delay: .7})
             .to(homeWrapper.current.firstChild, {x: '0', duration: 2.3, delay: -3})
             .to(homeWrapper.current.lastChild, {x: '0', duration: 2.3, delay: -3})
-            .from(logoAnimation.current, {scale: 0})
             .from(asideWrapper.current.children, {rotation: 60, x: '100', stagger: .3})
             .from(arrowBtn.current, {autoAlpha: 0, y: '+100', duration: 2})
             .fromTo(arrowBtn.current, {y: '0'}, {y: '+20', ease: 'power1.inOut', repeat: Infinity, yoyo: true})
@@ -42,14 +42,14 @@ const Home = () => {
     return (
         <>
             <div className="logo" ref={logoAnimation} >
-                <Link onClick={scrollToTop} className="logo-border">
+                <Link className="logo-border" onClick={scrollToTop} >
                     <div smooth={true}>
                         <p className="logo-name">ICM Inc.</p> 
                     </div>
                 </Link>
             </div>
             <div className="main-page">
-                <div ref={homeWrapper}>
+                <div ref={homeWrapper} className="tagline">
                     <p>Our design</p>
                     <p>Your <span>style</span></p>
                 </div>
@@ -57,12 +57,12 @@ const Home = () => {
             </div>
             <aside className="side-bar">
                 <ul ref={asideWrapper}>
-                    <li><NavLink to="/shop/all" target="_blank"><i className="fas fa-shopping-cart"></i></NavLink></li>
-                    <li><a href="https://www.facebook.com" rel="noreferrer" target="_blank"><i className="fab fa-facebook"></i></a></li>
-                    <li><a href="https://www.messenger.com" rel="noreferrer" target="_blank"><i className="fab fa-facebook-messenger"></i></a></li>
-                    <li><a href="https://www.instagram.com" rel="noreferrer" target="_blank"><i className="fab fa-instagram"></i></a></li>
-                    <li><a href="https://twitter.com" rel="noreferrer" target="_blank"><i className="fab fa-twitter-square"></i></a></li>
-                    <li><Link to="contact" smooth={true}><i className="fas fa-envelope"></i></Link></li>
+                    <li><NavLink to="/shop/all" target="_blank"><i className="fas fa-shopping-cart"></i><i className="hover fas fa-shopping-cart"></i></NavLink></li>
+                    <li><a href="https://www.facebook.com" rel="noreferrer" target="_blank"><i className="fab fa-facebook"></i><i className="hover fab fa-facebook"></i></a></li>
+                    <li><a href="https://www.messenger.com" rel="noreferrer" target="_blank"><i className="fab fa-facebook-messenger"></i><i className="hover fab fa-facebook-messenger"></i></a></li>
+                    <li><a href="https://www.instagram.com" rel="noreferrer" target="_blank"><i className="fab fa-instagram"></i><i className="hover fab fa-instagram"></i></a></li>
+                    <li><a href="https://twitter.com" rel="noreferrer" target="_blank"><i className="fab fa-twitter-square"></i><i className="hover fab fa-twitter-square"></i></a></li>
+                    <li><Link to="contact" smooth={true}><i className="fas fa-envelope"></i><i className="hover fas fa-envelope"></i></Link></li>
                 </ul>
             </aside>
         </>
